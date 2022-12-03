768755b002c0ffb498ebb8027563b3e5968ed238
Joe King <j.king92@gmail.com>
2022-12-03 14:30:57 -0700

    feat: Add create books functionality to authors books page

--- app/controllers/author_books_controller.rb
+++ app/controllers/author_books_controller.rb
@@ -3,4 +3,20 @@ class AuthorBooksController < ApplicationController
     @author = Author.find(params[:id])
     @books = Book.where(author_id: @author.id)
   end
+
+  def new
+    @author = Author.find(params[:id])
+  end
+
+  def create
+    author = Author.find(params[:id])
+    author.books.create(book_params)
+    redirect_to "/authors/#{ author.id }/books"
+  end
+
+  private
+  def book_params
+    cleansed_params = params.permit(:name, :available_as_audiobook, :page_length, :genre).to_h
+    cleansed_params.select { |_key, value| !value.empty? }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   post '/authors', to: 'authors#create'
   get '/authors/:id/edit', to: 'authors#edit'
   patch '/authors/:id', to: 'authors#update'
+  get '/authors/:id/books/new', to: 'author_books#new'
+  post '/authors/:id/books', to: 'author_books#create'
 
   get '/books', to: 'books#index'
   get '/books/:id', to: 'books#show'
